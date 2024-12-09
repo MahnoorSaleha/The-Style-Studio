05885c5e7edab22683daa9a9d34e44b24b8e745a
laibariaz33 <sp23-bse-086@cuilahore.edu.pk>
2024-12-09 14:11:38 +0500

    accessoryPage

--- TheStyleStudio/routes/user/user.products.controller.js
+++ TheStyleStudio/routes/user/user.products.controller.js
@@ -23,5 +23,25 @@ router.get('/clothes', async (req, res) => {
     res.status(500).send('Error retrieving products');
   }
 });
+router.get('/accessories', async (req, res) => {
+  try {
+   
+
+    const category = await Category.findOne({ name: 'Accessories' });
+    if (!category) {
+      console.error("Category 'Accessories' not found");
+      return res.status(404).send("Category 'Accessories' not found");
+    }
+
+    const accessories = await Product.find({ category: category._id })
+    if (!accessories.length) {
+      console.warn('No products found for the "Accessories" category');
+    }
+    res.render('accessories', { accessories: accessories });
+  } catch (error) {
+    console.error('Error fetching products:', error.message);
+    res.status(500).send('Error fetching products');
+  }
+});
 
 module.exports = router;
